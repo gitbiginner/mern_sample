f9cc8aedf4495aa6cf4aaa805abd8e2d2ff92d9c
gitbiginner <brackhorn@gmail.com>
2023-04-16 23:03:12 +0900

    npx create-react-app frontend

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const app = express()
 const port = 3000
+const Article = require('./models/Article')
 
-app.get('/', (req, res) => res.send(`Hello World`))
+app.delete('/articles/:id', (req, res) => {
+    Article.findOneAndRemove(
+        { _id: req.params.id },
+        (err, article) => {
+            res.send(article)
+        }
+    )
+})
+
+
+app.get('/articles', (req, res) => {
+    Article.find({}, (err, articles) => {
+        res.send(articles)
+    })
+})
+
+app.post('/articles', (req, res) => {
+    const newArticle = new Article(req.body)
+    newArticle.save((err, article) => {
+        res.send(article)
+    })
+})
+
+app.put('/articles/:id', (req, res) => {
+    Article.findOneAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        { new: true },
+        (err, article) => {
+            res.send(article)
+        }
+    )
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
