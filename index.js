0a2ef596681343e79231f3de068bf031931d0403
gitbiginner <brackhorn@gmail.com>
2023-05-01 23:41:46 +0900

    yahoo undici

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const calc  = 
+const {request} = require('undici');
+
+request('https://www.yahoo.co.jp')
+.then((res) => {
+    return res.body.text()
+})
+.then((body) => {
+    console.log(body);
+});
